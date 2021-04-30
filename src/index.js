3247113ec9dbb9014a70e035bf272c9d3f1df490
uhj1993 <dzzd777@naver.com>
2021-04-30 18:32:00 +0900

    [Update] react-router-dom 연결

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowerRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowerRouter>
+    <BrowserRouter>
       <App />
-    </BrowerRouter>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
