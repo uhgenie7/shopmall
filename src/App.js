ea54e6fbdaf353959b5d3f2f093ed51b0a8d895d
uhj1993 <dzzd777@naver.com>
2021-04-30 19:32:45 +0900

    [Update] react-router-dom 의 hook 다루기

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { Navbar, Nav, NavDropdown, Jumbotron, Button } from "react-bootstrap";
 import Data from "./data.js";
 import { Link, Route, Switch } from "react-router-dom";
+import Detail from "./Detail";
 
 function App() {
   let [shoes, shoesChange] = useState(Data);
@@ -14,8 +15,12 @@ function App() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link>
+              <Link to="/">Home</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/detail">Detail</Link>
+            </Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
@@ -30,43 +35,32 @@ function App() {
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-
-      <Route exact path="/">
-        <Jumbotron className="background">
-          <h1>MIITOPIA</h1>
-          <p>미토피아 체험판 배포 중!</p>
-          <p>
-            <Button variant="primary">자세히 알아보기</Button>
-          </p>
-        </Jumbotron>
-        <div className="container">
-          <div className="row">
-            {shoes.map((item, index) => {
-              return <ShoesList shoes={shoes[index]} i={index} key={index} />;
-            })}
-          </div>
-        </div>
-      </Route>
-      <Route path="/detail">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-6">
-              <img
-                src="https://codingapple1.github.io/shop/shoes1.jpg"
-                width="100%"
-              />
-            </div>
-            <div className="col-md-6 mt-4">
-              <h4 className="pt-5">상품명</h4>
-              <p>상품설명</p>
-              <p>120000원</p>
-              <button className="btn btn-danger">주문하기</button>
+      <Switch>
+        <Route exact path="/">
+          <Jumbotron className="background">
+            <h1>MIITOPIA</h1>
+            <p>미토피아 체험판 배포 중!</p>
+            <p>
+              <Button variant="primary">자세히 알아보기</Button>
+            </p>
+          </Jumbotron>
+          <div className="container">
+            <div className="row">
+              {shoes.map((item, index) => {
+                return <ShoesList shoes={shoes[index]} i={index} key={index} />;
+              })}
             </div>
           </div>
-        </div>
-      </Route>
-      {/* 이런 방식으로도 쓸 수 있다 */}
-      {/* <Route path="/hello" component={ Modal}></Route> */}
+        </Route>
+        <Route path="/detail">
+          <Detail />
+        </Route>
+        {/* 이런 방식으로도 쓸 수 있다 */}
+        {/* <Route path="/hello" component={ Modal}></Route> */}
+        <Route path="/:id">
+          <div>여긴 어떻게 오셨나요?</div>
+        </Route>
+      </Switch>
     </div>
   );
 }
