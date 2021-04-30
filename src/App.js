43bce904d0f04b322209fbeed08cf13c5955740e
uhj1993 <dzzd777@naver.com>
2021-04-30 16:34:46 +0900

    [Update] Jumbotron 컴포넌트 추가와 대략적인 레이아웃 추가

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import "./App.css";
-import {
-  Navbar,
-  Nav,
-  NavDropdown,
-  Form,
-  FormControl,
-  Button,
-} from "react-bootstrap";
+import { Navbar, Nav, NavDropdown, Jumbotron, Button } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
       <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">닌텐토 쇼핑몰</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
+          <Nav className="ml-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#link">Link</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
@@ -30,12 +23,49 @@ function App() {
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-success">Search</Button>
-          </Form>
         </Navbar.Collapse>
       </Navbar>
+
+      <Jumbotron className="background">
+        <h1>MIITOPIA</h1>
+        <p>미토피아 체험판 배포 중!</p>
+        <p>
+          <Button variant="primary">자세히 알아보기</Button>
+        </p>
+      </Jumbotron>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img
+              width="100%"
+              src="https://codingapple1.github.io/shop/shoes1.jpg"
+              alt="상품1"
+            />
+            <h4>상품명</h4>
+            <p>상품설명 & 가격</p>
+          </div>
+          <div className="col-md-4">
+            <img
+              width="100%"
+              src="https://codingapple1.github.io/shop/shoes2.jpg
+
+              "
+              alt="상품2"
+            />
+            <h4>상품명</h4>
+            <p>상품설명 & 가격</p>
+          </div>
+          <div className="col-md-4">
+            <img
+              width="100%"
+              src="https://codingapple1.github.io/shop/shoes3.jpg"
+              alt="상품3"
+            />
+            <h4>상품명</h4>
+            <p>상품설명 & 가격</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
