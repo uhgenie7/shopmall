dc65452478aedc3f4c3fdce33a92b113683f36ce
uhj1993 <dzzd777@naver.com>
2021-05-03 19:49:24 +0900

    [Update] 메인 페이지 퍼블리싱

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import "./App.css";
-import { Navbar, Nav, NavDropdown, Jumbotron, Button } from "react-bootstrap";
+import { Navbar, Nav, Jumbotron, Button } from "react-bootstrap";
 import Data from "./data.js";
 import { Link, Route, Switch } from "react-router-dom";
 import Detail from "./Detail";
 import axios from "axios";
-
+// props 상태관리 쉽게 하기
+let stockContext = React.createContext();
+// 같은 변수 값을 공유할 범위 생성
 function App() {
   let [shoes, shoesChange] = useState(Data);
   let [load, loadingChange] = useState(false);
@@ -23,66 +25,96 @@ function App() {
   }
   return (
     <div className="App">
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">닌텐토 쇼핑몰</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Link as={Link} to="/">
-              Home
-            </Nav.Link>
-            <Nav.Link as={Link} to="/detail">
-              Detail
-            </Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <header>
+        <Navbar expand="lg">
+          <h1 className="main-logo">
+            <Nav as={Link} to="/">
+              닌텐도
+            </Nav>
+          </h1>
+          <Navbar.Toggle aria-controls="navbar-light" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link as={Link} to="/">
+                Nintento switch
+              </Nav.Link>
+              <Nav.Link as={Link} to="/detail">
+                닌텐도 3DS
+              </Nav.Link>
+              <Nav.Link as={Link} to="/detail">
+                소프트웨어
+              </Nav.Link>
+              <Nav.Link as={Link} to="/detail">
+                고객지원
+              </Nav.Link>
+              <Nav.Link as={Link} to="/detail">
+                NEWS
+              </Nav.Link>
+            </Nav>
+            <Nav className="ml-auto d-md-flex d-none">
+              <Nav.Link as={Link} to="/">
+                <i class="fa fa-user"></i>
+              </Nav.Link>
+              <Nav.Link as={Link} to="/">
+                search
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </header>
       <Switch>
         <Route exact path="/">
-          <Jumbotron className="background">
-            <h1>MIITOPIA</h1>
-            <p>미토피아 체험판 배포 중!</p>
-            <p>
+          <section>
+            <Jumbotron className="background">
+              <h2>MIITOPIA</h2>
+              <p>미토피아 체험판 배포 중!</p>
               <Button variant="primary">자세히 알아보기</Button>
-            </p>
-          </Jumbotron>
-          <div className="container">
-            <div className="row">
-              {shoes.map((item, index) => {
-                return <ShoesList shoes={shoes[index]} i={index} key={index} />;
-              })}
+            </Jumbotron>
+          </section>
+          <section>
+            <div className="container">
+              <h2>NEWS & UPDATE</h2>
+              <stockContext.Provider value={stock}>
+                <div className="row">
+                  {shoes.map((item, index) => {
+                    return (
+                      <ShoesList shoes={shoes[index]} i={index} key={index} />
+                    );
+                  })}
+                </div>
+              </stockContext.Provider>
+              <button
+                className="btn btn-primary"
+                onClick={() => {
+                  axios.post("서버URL", { id: "admin", pw: 1234 });
+                  axios
+                    .get("https://codingapple1.github.io/shop/data2.json")
+                    .then((result) => {
+                      // loadingChange(!load);
+                      shoesChange([...shoes, ...result.data]);
+                    })
+                    .catch(() => {
+                      // loadingChange(!load);
+                      console.log("실패했습니다");
+                    });
+                }}
+              >
+                더보기
+              </button>
             </div>
-
-            <button
-              className="btn btn-primary"
-              onClick={() => {
-                axios.post("서버URL", { id: "admin", pw: 1234 });
-                axios
-                  .get("https://codingapple1.github.io/shop/data2.json")
-                  .then((result) => {
-                    // loadingChange(!load);
-                    shoesChange([...shoes, ...result.data]);
-                  })
-                  .catch(() => {
-                    // loadingChange(!load);
-                    console.log("실패했습니다");
-                  });
-              }}
-            >
-              더보기
-            </button>
-          </div>
+          </section>
+          <section>
+            <div className="container">
+              <h2>GAME TITLE</h2>
+              <button className="btn btn-primary">신상품</button>
+              <button className="btn btn-primary">SWITCH</button>
+              <button className="btn btn-primary">3DS</button>
+            </div>
+          </section>
+          <section>
+            <div className="container">배너자리</div>
+          </section>
+          <section>공지사항 자리</section>
         </Route>
         <Route path="/detail/:id">
           <Detail shoes={shoes} stock={stock} stockChange={stockChange} />
@@ -91,12 +123,15 @@ function App() {
           <div>여긴 어떻게 오셨나요?</div>
         </Route>
       </Switch>
+      <footer>footer자리</footer>
     </div>
   );
 }
 
 // component
 function ShoesList(props) {
+  let stocks = useContext(stockContext);
+
   return (
     <div className="col-md-4">
       <img
@@ -110,6 +145,7 @@ function ShoesList(props) {
       <p>
         {props.shoes.content} & {props.shoes.price}
       </p>
+      {stocks[props.i]}
     </div>
   );
 }
