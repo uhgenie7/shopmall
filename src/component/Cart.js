99515e084d7f0a03caaaf5b7fe6b1e8de2be613e
uhj1993 <dzzd777@naver.com>
2021-05-04 17:16:03 +0900

    [Update] redux로 props 관리하기

--- src/component/Cart.js
+++ src/component/Cart.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Table } from "react-bootstrap";
+import { connect } from "react-redux";
 
-function Cart() {
+function Cart(props) {
   return (
     <div>
       <Table responsive="lg">
@@ -16,7 +17,7 @@ function Cart() {
         <tbody>
           <tr>
             <td>1</td>
-            <td>Table cell</td>
+            <td>{props.state[0].name}</td>
             <td>Table cell</td>
             <td>Table cell</td>
           </tr>
@@ -26,4 +27,9 @@ function Cart() {
   );
 }
 
-export default Cart;
+function redux(state) {
+  return { state: state };
+}
+
+export default connect(redux)(Cart);
+// export default Cart;
