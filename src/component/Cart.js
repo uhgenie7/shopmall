d515878f8e585405df4dfe74a0b4d4916dca5aca
uhj1993 <dzzd777@naver.com>
2021-05-04 18:28:55 +0900

    [Update] 감소 버튼 개발

--- src/component/Cart.js
+++ src/component/Cart.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Table } from "react-bootstrap";
 import { connect } from "react-redux";
 
@@ -18,15 +18,36 @@ function Cart(props) {
           {props.state.map((item, index) => {
             return (
               <tr>
-                <td>{props.state[index].id + 1}</td>
-                <td>{props.state[index].name}</td>
-                <td>{props.state[index].quan}</td>
-                <td>Table cell</td>
+                <td>{item.id + 1}</td>
+                <td>{item.name}</td>
+                <td>{item.quan}</td>
+                <td>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => {
+                      props.dispatch({ type: "증가" });
+                    }}
+                  >
+                    +
+                  </button>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => {
+                      props.dispatch({ type: "감소" });
+                    }}
+                  >
+                    -
+                  </button>
+                </td>
               </tr>
             );
           })}
         </tbody>
       </Table>
+      <div className="my-alert-yellow">
+        <p>지금 구매하시면 신규 할인!</p>
+        <button>닫기</button>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -6,14 +6,30 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
-let store = createStore(() => {
-  return [
-    { id: 0, name: "미토피아", quan: 2 },
-    { id: 1, name: "New 포켓몬 스냅", quan: 4 },
-    { id: 2, name: "슈퍼 마리오 3D 월드 + 퓨리 월드", quan: 10 },
-    { id: 3, name: "링 피트 어드벤처", quan: 5 },
-  ];
-});
+
+let defAlert = true;
+
+let defState = [
+  { id: 0, name: "미토피아", quan: 2 },
+  { id: 1, name: "New 포켓몬 스냅", quan: 4 },
+  { id: 2, name: "슈퍼 마리오 3D 월드 + 퓨리 월드", quan: 10 },
+  { id: 3, name: "링 피트 어드벤처", quan: 5 },
+];
+function reducer(state = defState, action) {
+  if (action.type === "증가") {
+    let copy = [...state];
+    copy[0].quan++;
+    return copy;
+  } else if (action.type === "감소") {
+    let copy = [...state];
+    copy[0].quan--;
+    return copy;
+  } else {
+    return state;
+  }
+}
+
+let store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
